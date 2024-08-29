4f99ff5ef469409c85f4241402faf2670de23fb1
Babacar <bndoyecs@gmail.com>
2024-08-29 16:46:19 -0400

    cleanup

--- js/controllers/mainmenu.js
+++ js/controllers/mainmenu.js
@@ -4,12 +4,12 @@ const btn = document
   .getElementById("form")
   .addEventListener("submit", function (e) {
     e.preventDefault();
-    var formData = new FormData(form);
 
+    const formData = new FormData(form);
     const rows = formData.get("rows");
     const cols = formData.get("cols");
     const minesToPlace = formData.get("mines");
 
-    var display = new DisplayView(rows, cols, minesToPlace);
+    const display = new DisplayView(rows, cols, minesToPlace);
     display.displayBoard();
   });
--- js/models/main.js
+++
@@ -1,6 +0,0 @@
-import Minesweeper from "./minesweeper.js";
-
-const rows = 5;
-const cols = 7;
-const minesToPlace = 3;
-var TEST = new Minesweeper(rows, cols, minesToPlace);
