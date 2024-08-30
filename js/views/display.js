846ed0ac0646ea4153f5dc8a95347579b2924e6d
Babacar <bndoyecs@gmail.com>
2024-08-30 15:02:50 -0400

    separating some of the logic

--- js/views/display.js
+++ js/views/display.js
@@ -5,45 +5,40 @@ export default class DisplayView {
     this.rows = rows;
     this.cols = cols;
     this.minesToPlace = minesToPlace;
-    this.Minesweeper = new Minesweeper(rows, cols, minesToPlace);
+    this.minesweeper = new Minesweeper(rows, cols, minesToPlace);
   }
 
   displayBoard() {
-    let lastClicked;
-    let grid = clickableGrid(this.rows, this.cols, function (el, row, col, i) {
+    let grid = this.clickableGrid(function (el, row, col, action) {
+      // This will be used to call the minesweeper model
       console.log("You clicked on element:", el);
       console.log("You clicked on row:", row);
       console.log("You clicked on col:", col);
-      console.log("You clicked on item #:", i);
-
+      console.log("You did this action:", action);
       el.className = "clicked";
-      // if (lastClicked) lastClicked.className = "";
-      // lastClicked = el;
     });
-
     document.body.appendChild(grid);
+  }
 
-    function clickableGrid(rows, cols, callback) {
-      let i = 0;
-      let grid = document.createElement("table");
-      grid.className = "grid";
-      for (let r = 0; r < rows; ++r) {
-        let tr = grid.appendChild(document.createElement("tr"));
-        for (let c = 0; c < cols; ++c) {
-          let cell = tr.appendChild(document.createElement("td"));
-          cell.innerHTML = ++i;
-          cell.addEventListener(
-            "click",
-            (function (el, r, c, i) {
-              return function () {
-                callback(el, r, c, i);
-              };
-            })(cell, r, c, i),
-            false
-          );
-        }
+  clickableGrid(callback) {
+    let grid = document.createElement("table");
+    grid.className = "grid";
+    for (let r = 0; r < this.rows; ++r) {
+      let tr = grid.appendChild(document.createElement("tr"));
+      for (let c = 0; c < this.cols; ++c) {
+        let cell = tr.appendChild(document.createElement("td"));
+        cell.innerHTML = this.minesweeper.getCellValue(r, c);
+        cell.addEventListener(
+          "click",
+          (function (el, r, c, action) {
+            return function () {
+              callback(el, r, c, action);
+            };
+          })(cell, r, c, "rightclick"),
+          false
+        );
       }
-      return grid;
     }
+    return grid;
   }
 }
