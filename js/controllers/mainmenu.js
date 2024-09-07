3e71646123a86ef4fd5eff3e462fc0eab49bb963
Babacar <bndoyecs@gmail.com>
2024-09-07 14:53:46 -0400

    code cleanup a little

--- js/controllers/mainmenu.js
+++ js/controllers/mainmenu.js
@@ -1,15 +1,13 @@
 import ViewHandler from "../views/viewhandler.js";
 
-const btn = document
-  .getElementById("form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const formData = new FormData(form);
-    const rows = formData.get("rows");
-    const cols = formData.get("cols");
-    const minesToPlace = formData.get("mines");
+  const formData = new FormData(form);
+  const rows = formData.get("rows");
+  const cols = formData.get("cols");
+  const minesToPlace = formData.get("mines");
 
-    const display = new ViewHandler(rows, cols, minesToPlace);
-    display.displayBoard();
-  });
+  const display = new ViewHandler(rows, cols, minesToPlace);
+  display.displayBoard();
+});
--- js/models/minesweeper.js
+++ js/models/minesweeper.js
@@ -115,7 +115,7 @@ export default class Minesweeper {
 
   expandCell(row, col) {
     // Case 1: If a user clicks a number, reveal that number
-    // Case 2: If a user clicks a 0/empty space, expand out in all directions until you hit a number
+    // Case 2: If a user clicks a 0/empty space, expand out in all directions until you hit a number (most likely using bfs algorithm)
     // Case 3: If a user clicks a mine, game over :)
     // One thing I wonder is, if a user sets a cell to a flag, and they click a zero cell do I expand till the flag or stil expand the value under the flag
   }
