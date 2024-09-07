3281f0021e9a3432772cea6291e85dd92dd57bf6
Babacar <bndoyecs@gmail.com>
2024-09-06 21:27:15 -0400

    game consideration

--- js/models/minesweeper.js
+++ js/models/minesweeper.js
@@ -117,5 +117,6 @@ export default class Minesweeper {
     // Case 1: If a user clicks a number, reveal that number
     // Case 2: If a user clicks a 0/empty space, expand out in all directions until you hit a number
     // Case 3: If a user clicks a mine, game over :)
+    // One thing I wonder is, if a user sets a cell to a flag, and they click a zero cell do I expand till the flag or stil expand the value under the flag
   }
 }
