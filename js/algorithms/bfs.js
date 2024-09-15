b28bb06eb6f033a2bddff7885429fa92bd11cfd6
Babacar <bndoyecs@gmail.com>
2024-09-15 16:49:49 -0400

    algorithm

---
+++ js/algorithms/bfs.js
@@ -0,0 +1,13 @@
+// Eventually will try to incorporate other algorithms for the computer aspect of this game
+// Also will use inheritance for various algorithms
+export default class BreadthFirstSearch {
+  constructor() {}
+
+  expandCell(row, col) {
+    // Case 1: If a user clicks a number, reveal that number
+    // Case 2: If a user clicks a 0/empty space, expand out in all directions until you hit a number (most likely using bfs algorithm)
+    // Case 3: If a user clicks a mine, game over :)
+    // One thing I wonder is, if a user sets a cell to a flag, and they click a zero cell do I expand till the flag or stil expand the value under the flag
+    console.log("hi");
+  }
+}
