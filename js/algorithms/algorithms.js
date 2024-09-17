725907a1e99087845bbfd788d23d98e2fc70b969
Babacar <bndoyecs@gmail.com>
2024-09-17 19:31:04 -0400

    BFS Algorithm

--- js/algorithms/algorithms.js
+++ js/algorithms/algorithms.js
@@ -3,16 +3,49 @@ export default class Algorithms {
   constructor(mineValue) {
     this.mineValue = mineValue; // value used to indicate a mine
   }
-
-  // Case 2: If a user clicks a number, reveal that number
-  // Case 3: If a user clicks a 0/empty space, expand out in all directions until you hit a number (most likely using bfs algorithm)
   // One thing I wonder is, if a user sets a cell to a flag, and they click a zero cell do I expand till the flag or stil expand the value under the flag
 
   bfsExpandCell(board, row, col) {
     // Case 1: If a user clicks a mine, send them to the death realm (game over) :)
     if (board[row][col].value == this.mineValue) return false;
 
-    console.log("No mine hit.");
+    // Case 2: If a user clicks a number greater than 0, reveal that number, and return
+    // Case 3: If a user clicks a 0/empty space, expand out in all directions until you hit a number (most likely using bfs algorithm)
+    const directions = [
+      // [Row, Col] or [Y-Coordinate, X-Coordinate]
+      [0, -1], // Left
+      [0, 1], // Right
+      [-1, 0], // Up
+      [1, 0], // Down
+      [-1, -1], // Up Left
+      [-1, 1], // Up Right
+      [1, -1], // Down Left
+      [1, 1], // Down Right
+    ];
+
+    const rows = board.length;
+    const cols = board[0].length;
+    const queue = [[row, col]];
+
+    while (queue.length) {
+      const [r, c] = queue.shift();
+      board[r][c].isOpen = true;
+
+      if (board[r][c].value > 0) continue;
+
+      for (const direction of directions) {
+        const newRow = direction[0] + r;
+        const newCol = direction[1] + c;
+
+        if (newRow < 0 || newCol < 0 || newRow >= rows || newCol >= cols)
+          continue;
+
+        if (!board[newRow][newCol].isOpen) {
+          queue.push([newRow, newCol]);
+        }
+      }
+    }
+
     return true;
   }
 }
