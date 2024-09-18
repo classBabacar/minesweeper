77309cd1048d4bb57b28c137810d9799a2e857c7
Babacar <bndoyecs@gmail.com>
2024-09-17 20:03:50 -0400

    removing todo's

--- js/algorithms/algorithms.js
+++ js/algorithms/algorithms.js
@@ -3,14 +3,13 @@ export default class Algorithms {
   constructor(mineValue) {
     this.mineValue = mineValue; // value used to indicate a mine
   }
-  // One thing I wonder is, if a user sets a cell to a flag, and they click a zero cell do I expand till the flag or stil expand the value under the flag
 
   bfsExpandCell(board, row, col) {
     // Case 1: If a user clicks a mine, send them to the death realm (game over) :)
     if (board[row][col].value == this.mineValue) return false;
 
     // Case 2: If a user clicks a number greater than 0, reveal that number, and return
-    // Case 3: If a user clicks a 0/empty space, expand out in all directions until you hit a number (most likely using bfs algorithm)
+    // Case 3: If a user clicks a 0/empty space, expand out in all directions until you hit a number
     const directions = [
       // [Row, Col] or [Y-Coordinate, X-Coordinate]
       [0, -1], // Left
